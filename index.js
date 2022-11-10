0350ff28ec2245b027a85ef10929ba0e7dce1aca
Angela <balderasangela@gmail.com>
2022-11-10 13:34:24 -0500

    COMPLETED ASSIGNMENT

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+function findMatching(names,string) {
+    const stringDriver = string.toUpperCase()
+    const driverNames = names.filter (driver => driver.toUpperCase() === stringDriver)
+    return driverNames
+    }
+
+
+
+function fuzzyMatch(names,string) {
+    const fuzzyDrivers = names.filter(function(driver) {
+        return driver.startsWith(string)
+    })
+    return fuzzyDrivers
+}
+
+function matchName(driverObjects,string) {
+    const matchedDrivers = driverObjects.filter(function(driver) {
+        return driver.name === string
+    })
+    return matchedDrivers
+}
